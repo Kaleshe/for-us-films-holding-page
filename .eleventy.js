d826ad5c7258fc0813a94a077c6b22eeb8dcf0d2
Kaleshe <kalesheav@gmail.com>
2021-11-01 12:57:58 +0000

    Added svg logo and elevator pitch

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy(".htaccess");
+  eleventyConfig.addPassthroughCopy("google54f80e502fda2bf6.html");
 
   return {
     dir: {
